fea611cea1c04691b1929db5616ebe1960515d14
Daniel Rocha <daniel.rochats@gmail.com>
2025-09-24 20:01:51 -0300

    Atualizar o tables.go

--- go/src/models/tables.go
+++ go/src/models/tables.go
@@ -3,14 +3,11 @@ package models
 import "time"
 
 type Table struct {
-	ID          uint       `gorm:"primaryKey"`
-	Number      int        `gorm:"index;not null"`          // número fixo da mesa
-	LastOrderAt *time.Time `gorm:"index"`
-	OpenedAt    *time.Time `gorm:"index"`
-	ClosedAt    *time.Time `gorm:"index"`
-	ServiceID   *uint      `gorm:"index"`
-	IsOpen      bool       `gorm:"not null;default:false;index"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          uint       `gorm:"primaryKey;autoIncrement"`
+	Number      int        `gorm:"not null"` // número fixo da mesa
+	LastOrderAt *time.Time
+	OpenedAt    *time.Time
+	ClosedAt    *time.Time
+	ServiceID   *uint
+	IsOpen      bool       `gorm:"not null;default:false"`
 }
-

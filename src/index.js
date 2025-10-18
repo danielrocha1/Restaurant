1f37f39bc9316bb1b03f4d51be286e90bb51e01d
Daniel Rocha <daniel.rochats@gmail.com>
2025-10-18 16:51:38 -0500

    wsContext e provider

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider } from "./context/cartContext";
+import { WSProvider } from "./context/wsContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <CartProvider>
-    <App />
-  </CartProvider>
+  <WSProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </WSProvider>
+
 );

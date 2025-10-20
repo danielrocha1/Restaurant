55ea3310e9a82dd3d412f9c6c9b7971888eaa96b
Daniel Rocha <daniel.rochats@gmail.com>
2025-10-20 20:29:11 -0300

    Atualizar o cart.js

--- src/cart/cart.js
+++ src/cart/cart.js
@@ -65,7 +65,7 @@ const Cart = () => {
               body: JSON.stringify({
                 qrCode: decodedText,
                 items: productList,
-                total: Number(totalPrice.toFixed(2))
+                total: totalPrice,
               }),
             });
 

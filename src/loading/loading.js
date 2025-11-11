24b2b969507f2d8c21b14a9453bd07135c5eb5f8
Daniel Rocha <daniel.rochats@gmail.com>
2025-11-11 01:45:15 -0300

    teste loading

--- src/loading/loading.js
+++ src/loading/loading.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Lottie from "lottie-react";
-import ramenSushiAnimation from "./Ramen&Sushi.json"; // ajuste o caminho conforme onde você salvou o JSON
+import ramenSushiAnimation from "../assets/Ramen&Sushi.json";
 import "./loading.css";
 
 const LoadingScreen = () => {
+  const [show, setShow] = useState(false);
+
+  // ⏱️ Delay de 0.75s antes de mostrar o Lottie
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), 750);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="loading-overlay">
+    <div className="loading-overlay gradient-bg">
       <div className="loader-container">
-        <Lottie
-          animationData={ramenSushiAnimation}
-          loop
-          autoplay
-          style={{ width: 200, height: 200 }}
-        />
+        {show && (
+          <Lottie
+            animationData={ramenSushiAnimation}
+            loop
+            autoplay
+            style={{ width: 500, height: 500 }}
+          />
+        )}
       </div>
     </div>
   );
